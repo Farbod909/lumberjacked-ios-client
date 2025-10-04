6fc75b0d0791bfea4968e9dca590787ca8ce2d3e
Farbod909 <rafezyfarbod@gmail.com>
2025-10-04 09:45:23 -0700

    Always show day of the week for workouts

--- Lumberjacked/Lumberjacked/Models/Workout.swift
+++ Lumberjacked/Lumberjacked/Models/Workout.swift
@@ -23,28 +23,18 @@ struct Workout: Codable, Hashable {
 
         let now = Date()
         let calendar = Calendar.current
-        let components = calendar.dateComponents([.day], from: start_timestamp, to: now)
-
-        // Check if the date is within the last 14 days (13 days ago or less)
-        if let days = components.day, days < 14 {
-            let dateFormatter = DateFormatter()
-
-            // Handle "Today" and "Yesterday" first
-            if calendar.isDateInToday(start_timestamp) {
-                return "Today"
-            }
-            if calendar.isDateInYesterday(start_timestamp) {
-                return "Yesterday"
-            }
-
-            dateFormatter.dateFormat = "EEEE, MMMM d"
-            dateFormatter.timeZone = .current
-            return dateFormatter.string(from: start_timestamp)
-        }
 
         let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .medium
-        dateFormatter.timeStyle = .none
+
+        // Handle "Today" and "Yesterday" first
+        if calendar.isDateInToday(start_timestamp) {
+            return "Today"
+        }
+        if calendar.isDateInYesterday(start_timestamp) {
+            return "Yesterday"
+        }
+
+        dateFormatter.dateFormat = "EEEE, MMMM d"
         dateFormatter.timeZone = .current
         return dateFormatter.string(from: start_timestamp)
     }
